d913cfd38f756fa1a0321aee8e017c574d173731
Pijus Merkeliunas <pijus.merkeliunas@gmail.com>
2023-11-19 20:05:22 +0200

    Fixed "Create" button bug.

--- wizard/print_data.py
+++ wizard/print_data.py
@@ -9,15 +9,17 @@ class printData(models.TransientModel):
 
     # date = fields.Many2one('lib.library','create_date')"""
 
-    def print_required_data(self):
-        return self.env.ref('library.report_print_data').report_action(self)
-        """
-        if context is None:
-            context = {}
+    @api.model
+    def print_required_data(self, cr, uid, ids, context=None):
+        #return self.env.ref('library.report_print_data').report_action(self)
+
+        if ids:
+            if not isinstance(ids, list):
+                ids = [ids]
+            context = dict(context or {}, active_ids=ids, active_model=self._name)
 
         data = {}
-        data['ids'] = context.get('active_ids', [])
-        data['model'] = context.get('active_model', 'ir.ui.menu')
-        return self.pool['report'].get_action(cr, uid, [], 'library.print_required_data', data=data, context=context)
+        data['ids'] = context.get('name', [])
+        data['model'] = context.get('lib.library', 'ir.ui.menu')
 
-    """
+        return self.pool['report'].get_action(cr, uid, [], 'library.report_print_data', data=data, context=context)
