1d481d6137e407ccc5f971b88ddaa8d5e561fc5a
Pijus Merkeliunas <pijus.merkeliunas@gmail.com>
2023-11-19 19:05:26 +0200

    Updated manifest. Started print button.

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,18 @@
 {
-    'name': "Libraryssssssssssss",
+    'name': "Library by Pijus",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Library module""",
 
     'description': """
-        Long description of module's purpose
+        In total there is 3 views in this module, 2 in file views/views.xml and one in wizard/print_data_view.xml.
+        There are also 3 actions in this module, each assigned to a specific view. The menu bar is also defined in
+        views/views.xml. This module uses 2 models. The folders controllers and demo were not used, but were generated
+        automatically with "odoo scaffold" command.
     """,
 
-    'author': "Pijus",
-    'website': "http://www.yourcompany.com",
+    'author': "Pijus Merkeliūnas",
+    'website': "https://www.linkedin.com/in/pijus-merkeliunas/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -25,12 +27,12 @@
     'data': [
         'security/ir.model.access.csv',
         'wizard/print_data_view.xml',
+        'wizard/templates.xml',
         'views/views.xml',
-        'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    #'demo': [
+    #    'demo/demo.xml',
+    #],
     'application': True,
 }
--- wizard/print_data.py
+++ wizard/print_data.py
@@ -7,4 +7,17 @@ class printData(models.TransientModel):
     from_date = fields.Datetime('begin_date')
     to_date = fields.Datetime('end_date')
 
-    # date = fields.Many2one('lib.library','create_date')
+    # date = fields.Many2one('lib.library','create_date')"""
+
+    def print_required_data(self):
+        return self.env.ref('library.report_print_data').report_action(self)
+        """
+        if context is None:
+            context = {}
+
+        data = {}
+        data['ids'] = context.get('active_ids', [])
+        data['model'] = context.get('active_model', 'ir.ui.menu')
+        return self.pool['report'].get_action(cr, uid, [], 'library.print_required_data', data=data, context=context)
+
+    """
